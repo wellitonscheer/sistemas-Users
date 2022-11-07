5583f541e99c9210b5a6c0cfb8ea2c7016ef06e9
wellitonscheer <wellitonscheer@gmail.com>
2022-11-07 17:21:16 -0300

    dynamo e lambda(inline) no mesmo cloudformation funcionando

--- cli/comands.sh
+++ cli/comands.sh
@@ -4,8 +4,9 @@ source $script_dir/../.env
 
 dir_output_file=$script_dir/temp/packged.yaml
 
-aws cloudformation package --template-file $script_dir/../cloudFormation/creatingLambdaFunction.yaml --s3-bucket $DeployBucketName --output-template-file $dir_output_file --region $AWS_REGION
+aws cloudformation package --template-file $script_dir/../cloudFormation/CLsistemasUsers.yaml --s3-bucket $DeployBucketName --output-template-file $dir_output_file --region $AWS_REGION
 
 aws cloudformation deploy --template-file $dir_output_file --stack-name sistemas-users-stack --region $AWS_REGION
 
 cat $dir_output_file
+rm -f $dir_output_file
